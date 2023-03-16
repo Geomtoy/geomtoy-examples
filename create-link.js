510cc0d11604a2d1a1e8c1e9c049323b4adf59fd
FishEleven <mog1347@qq.com>
2023-03-16 16:38:10 +0800

    build: Update repo compilation, debugging, maintenance matters
    -

---
+++ create-link.js
@@ -0,0 +1,4 @@
+const symlinkDir = require("symlink-dir");
+symlinkDir("geomtoy/packages/util", "node_modules/@geomtoy/util");
+symlinkDir("geomtoy/packages/core", "node_modules/@geomtoy/core");
+symlinkDir("geomtoy/packages/view", "node_modules/@geomtoy/view");
